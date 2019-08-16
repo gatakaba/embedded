57e656ea8b4f8484a8041b9d660e14cfd61d5a32
gatakaba <m07112@gmail.com>
2019-08-16 13:32:36 +0900

    apply fomatter

--- sht-35.py
+++ sht-35.py
@@ -1,28 +1,22 @@
 # coding: utf-8
 
 
-
 import smbus
 import time
-
 
 
 addr = 0x45
 i2c = smbus.SMBus(1)
 
 
-
-
-def calculation(t_data,h_data):
-    Humidity = 100.0*float(h_data)/65535.0
-    Temperature = -45.0 + 175.0*float(t_data)/65535.0
-    return Humidity,Temperature
-
-
+def calculation(t_data, h_data):
+    Humidity = 100.0 * float(h_data) / 65535.0
+    Temperature = -45.0 + 175.0 * float(t_data) / 65535.0
+    return Humidity, Temperature
 
 
 while True:
-    i2c.write_i2c_block_data(addr,0x2C,[0x06])
+    i2c.write_i2c_block_data(addr, 0x2C, [0x06])
     time.sleep(0.05)
     data = i2c.read_i2c_block_data(addr, 0x00, 12)
     time.sleep(0.05)
@@ -32,4 +26,4 @@ while True:
     # Devide data into counts Humidity
     h_data = data[3] << 8 | data[4]
 
-    print(calculation(t_data,h_data))
+    print(calculation(t_data, h_data))
